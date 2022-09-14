39dd2407f7c76b84acf32571cd325d5422ecff82
Calebe Felipe Alves Freitas <fcalebe7@gmail.com>
2022-09-14 10:29:09 -0300

    fix: solving logic problem in playing again

--- main.js
+++ main.js
@@ -5,13 +5,8 @@ const inputNumber = document.querySelector("#inputNumber")
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 const h2 = document.querySelector(".screen2 h2")
-const randomNumber = Math.round(Math.random() * 10)
+let randomNumber = Math.round(Math.random() * 10)  
 let xAttempts = 1
-
-//Eventos
-buttonTry.addEventListener('click', handleTryClick)
-buttonReset.addEventListener('click', handleResetClick)
-document.addEventListener('keydown', keydownEnter)
 
 //Funções
 const handleTryClick = (event) => { 
@@ -29,11 +24,12 @@ const handleTryClick = (event) => {
 const handleResetClick = () => {
     toggleScreen()
     xAttempts = 1
+    randomNumber = Math.round(Math.random() * 10)
 }
 
 const toggleScreen = () => {
-    screen1.classList.toggle(".hide")
-    screen2.classList.toggle(".hide")
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
 }
 
 const keydownEnter = (e) => {
@@ -41,4 +37,9 @@ const keydownEnter = (e) => {
         handleResetClick()
     }
 }
+
+//Eventos
+buttonTry.addEventListener('click', handleTryClick)
+buttonReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', keydownEnter)
 
